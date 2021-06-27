eeb15d79a017fd3a1a5cfc180346c740b233f73e
Michael Jacinto <atmwhockey@hotmail.com>
2021-06-27 15:44:14 -0700

    Finish basic to do app

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,18 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-function TodoForm() {
-  const [input, setInputs] = useState("");
+function TodoForm(props) {
+  // allows us to edit current to do
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    props.onSubmit({
+      id: Math.floor(Math.random() * 1000),
+      text: input,
+    });
+
+    setInput("");
+  };
+
   return (
     <div>
-      <form className="todo-form">
-        <input
-          type="text"
-          placeholder="Add a todo.."
-          value={input}
-          name="text"
-          className="todo-input"
-        />
-        <button className="todo-button">Add todo</button>
+      <form className="todo-form" onSubmit={handleSubmit}>
+        {props.edit ? (
+          <>
+            <input
+              type="text"
+              placeholder="Update your item"
+              value={input}
+              name="text"
+              className="todo-input edit"
+              onChange={handleChange}
+              ref={inputRef}
+            />
+            <button className="todo-button edit">Update</button>
+          </>
+        ) : (
+          <>
+            <input
+              type="text"
+              placeholder="Update your item"
+              value={input}
+              name="text"
+              className="todo-input"
+              onChange={handleChange}
+              ref={inputRef}
+            />
+            <button className="todo-button">Add todo</button>
+          </>
+        )}
       </form>
     </div>
   );
