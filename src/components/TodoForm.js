d741000761716e5fec4d58e062b8df0cc6247cc2
Michael Jacinto <atmwhockey@hotmail.com>
2021-06-27 00:02:41 -0700

    Initial commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+function TodoForm() {
+  const [input, setInputs] = useState("");
+  return (
+    <div>
+      <form className="todo-form">
+        <input
+          type="text"
+          placeholder="Add a todo.."
+          value={input}
+          name="text"
+          className="todo-input"
+        />
+        <button className="todo-button">Add todo</button>
+      </form>
+    </div>
+  );
+}
+
+export default TodoForm;
