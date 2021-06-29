5d3ef8947d35b970f771a9c7262f80b9ecead0e0
Michael Jacinto <atmwhockey@hotmail.com>
2021-06-28 22:17:02 -0700

    Edit app css

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,3 +1,4 @@
+import { Badge, Checkbox } from "evergreen-ui";
 import React, { useState } from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
@@ -8,6 +9,8 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
     id: null,
     value: "",
   });
+
+  const [checked, setChecked] = useState(false);
 
   const submitUpdate = (value) => {
     updateTodo(edit.id, value);
@@ -22,24 +25,31 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
   }
 
   return todos.map((todo, index) => (
-    <div
-      className={todo.isComplete ? "todo-row complete" : "todo-row"}
-      key={index}
-    >
-      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-        {todo.text}
-      </div>
-      <div className="icons">
-        <RiCloseCircleLine
-          onClick={() => removeTodo(todo.id)}
-          className="delete-icon"
+    <>
+      <div
+        className={todo.isComplete ? "todo-row complete" : "todo-row"}
+        key={index}
+      >
+        <Checkbox
+          paddingLeft={10}
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
         />
-        <TiEdit
-          onClick={() => setEdit({ id: todo.id, value: todo.text })}
-          className="edit-icon"
-        />
+        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+          <Badge color="blue">{todo.text}</Badge>
+        </div>
+        <div className="icons">
+          <RiCloseCircleLine
+            onClick={() => removeTodo(todo.id)}
+            className="delete-icon"
+          />
+          <TiEdit
+            onClick={() => setEdit({ id: todo.id, value: todo.text })}
+            className="edit-icon"
+          />
+        </div>
       </div>
-    </div>
+    </>
   ));
 }
 
